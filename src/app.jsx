fc426b7e874ecc39e36655762b6cced1c712b516
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-07 11:54:19 -0300

    created App.jsx file

---
+++ src/app.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+import Primeiro from './components/basics/Primeiro'
+import ComParametro from './components/basics/ComParametro'
+import Fragmento  from './components/basics/Fragmento'
+
+
+export default function App(props){
+
+    <div id="app">
+        <Primeiro></Primeiro>
+
+        <ComParametro
+            titulo="Situação do Aluno"
+            aluno="Pedro silva"
+            nota={9.3} />
+
+        <Fragmento
+
+            
+        />
+
+
+    </div>
+
+}
