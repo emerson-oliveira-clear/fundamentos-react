cbe1b57efcc62904831077e47c0e54591021efc0
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-07 11:55:35 -0300

    update: removing and transferring components

--- src/index.js
+++ src/index.js
@@ -3,25 +3,15 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-import Primeiro from './components/basics/Primeiro'
-import ComParametro from './components/basics/ComParametro'
-import Fragmento  from './components/basics/Fragmento'
+import App from './app'
 
 ReactDOM.render(
-    <div id="app">
-        <Primeiro></Primeiro>
-
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro silva"
-            nota={9.3} />
-
-        <Fragmento
-
-            
-        />
 
 
-    </div>,
+    <App 
+    
+    />,
+
+
     document.getElementById('root')
 )
