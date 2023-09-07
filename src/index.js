6f96a1d9b119fb5e1015922e8c39cec4ddd271bc
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-07 10:05:32 -0300

    update: adding index.css import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+import './index.css'
+
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-const tag =  <strong>olá react</strong> 
-
+const tag =  <strong>Olá react</strong> 
 
 ReactDOM.render(
     <div>
         <strong>Olá react </strong>
-        
+        <br></br>
+        <br></br>
         {tag}
+
     </div>,
      document.getElementById('root')
 )
