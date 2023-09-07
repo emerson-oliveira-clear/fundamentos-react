6882237aa0af9c44d9323a6108845ee00dfe1afc
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-07 12:47:30 -0300

    update: adding Aleatorio import

--- src/app.jsx
+++ src/app.jsx
@@ -5,11 +5,17 @@ import Primeiro from './components/basics/Primeiro'
 import ComParametro from './components/basics/ComParametro'
 import Fragmento from './components/basics/Fragmento'
 
+import Aleatorio from "./components/basics/Aleatorio";
+
 
 export default (_) => {
 
     <div id="app">
         <h1>Fundamento React 2(arrow)</h1>
+
+        <Aleatorio
+            
+        />
 
         <Fragmento
         />
