8ea2ba59bbbb0d3da624bd50038cebd6bda4c993
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-07 10:41:15 -0300

    update: adding ComParametro import

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basics/Primeiro'
-
-
+import ComParametro from './components/basics/ComParametro'
 
 ReactDOM.render(
     <div>
-        
         <Primeiro></Primeiro>
-
+        <ComParametro></ComParametro>
     </div>,
      document.getElementById('root')
 )
