9c936f3eff720f40f61967c792ee1fc5163ed5b4
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-11 12:30:00 -0300

    UPDATECReated new Method

--- src/components/condicional/If.js
+++ src/components/condicional/If.js
@@ -1,14 +1,33 @@
 
+export default props => {
+    const elseChild = props.children.filter(child => {
+
+        return child.type && child.type.name === 'Else'
+
+    })[0]
+
+    const ifChildren = props.children.filter(child => {
+
+        return child !== elseChild
+
+    })
 
 
 
-export default props =>{
 
-    if(props.test){
+    if (props.test) {
 
-        return props.children
+        return ifChildren
+        
+    } else if (elseChild) {
+
+        return elseChild
+
     } else {
+
         return false
     }
 
 }
+
+export const Else = props => props.children
