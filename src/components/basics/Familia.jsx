76d2c541478696fd43d3b9a7b103cb951de06817
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-11 10:17:37 -0300

    UPDATE: Using react.children.map

--- src/components/basics/Familia.jsx
+++ src/components/basics/Familia.jsx
@@ -3,10 +3,18 @@ import React,{cloneElement} from "react";
 
 export default props =>{
 
+  
+
     return(
 
         <div>
-            {cloneElement(props.children,{...props})}
+            {
+            React.Children.map(props.children,child =>{
+
+                return cloneElement(child,props)
+
+            })
+            }
            
 
         </div>
