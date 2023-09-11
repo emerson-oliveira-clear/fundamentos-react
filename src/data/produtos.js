5f9b6b1608fa6342e9722dc5feab803e3b9c7eb3
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-11 11:36:03 -0300

    UPDATE: file created and content inserted

---
+++ src/data/produtos.js
@@ -0,0 +1,15 @@
+const produtos = [
+
+    {id: 1,nome:'Morango' , preco:9.99},
+    {id: 2,nome:'Uva' , preco: 7.89},
+    {id: 3,nome:'Pera' , preco:5.59},
+    {id: 4,nome:'Maça' , preco:1.99},
+    {id: 5,nome:'Goiaba' , preco:8.99},
+    {id: 6,nome:'Melancia' , preco:7.89},
+    {id: 7,nome:'Abacate' , preco:12.99},
+    {id: 8,nome:'Abacaxi' , preco:5.49},
+    {id: 9,nome:'Maracuja' , preco:15.98}
+
+]
+
+export default produtos
