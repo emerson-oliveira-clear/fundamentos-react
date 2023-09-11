9eb6e5d9c244a090e3c0bd6d188ed95db156ddd7
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-11 09:34:08 -0300

    UPDATE: adding colorson cards

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -7,8 +7,16 @@ import React from "react"
 
 export default props => {
 
+    const cardStyle = {
+
+        backgroundColor:props.color || '#206fd7', 
+        borderColor: props.color || '#206fd7',
+
+
+    }
+
     return (
-        <div className="Card">
+        <div className="Card" style={cardStyle}>
             <div className='Title'>{props.titulo}</div>
             <div className='Content'>{props.children}</div>
         </div>
