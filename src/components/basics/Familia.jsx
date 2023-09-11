9367b7b149d544fb41e4aa19e6ca9c08dd22cabd
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-11 09:56:46 -0300

    UPDATE: assigning values ​​to names

--- src/components/basics/Familia.jsx
+++ src/components/basics/Familia.jsx
@@ -1,1 +1,19 @@
 import React from "react";
+
+import FamiliaMembro from "./FamiliaMembro";
+
+export default props =>{
+
+    return(
+
+        <div>
+            <FamiliaMembro nome='Pedro' sobrenome={props.sobrenome} />
+
+            <FamiliaMembro nome='Ana'  {...props}/>
+
+            <FamiliaMembro nome='Gustavo' sobrenome='Casão' />
+
+        </div>
+
+    )
+}
