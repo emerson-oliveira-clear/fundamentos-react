e32de8e0a3f1d06ae901a1e7e310b3ad43bf8df5
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-11 10:09:28 -0300

    UPDATE: remove children to use cloneElement

--- src/components/basics/Familia.jsx
+++ src/components/basics/Familia.jsx
@@ -1,19 +1,16 @@
-import React from "react";
+import React,{cloneElement} from "react";
 
-import FamiliaMembro from "./FamiliaMembro";
 
 export default props =>{
 
     return(
 
         <div>
-            <FamiliaMembro nome='Pedro' sobrenome={props.sobrenome} />
-
-            <FamiliaMembro nome='Ana'  {...props}/>
-
-            <FamiliaMembro nome='Gustavo' sobrenome='Casão' />
+            {cloneElement(props.children,{...props})}
+           
 
         </div>
 
     )
 }
+
