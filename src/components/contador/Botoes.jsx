131ebfc6ca8187cf4ac376521e409cbd50f6599f
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-12 10:46:29 -0300

    UPDATE: refactoring methods

---
+++ src/components/contador/Botoes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default props => {
+
+    return (
+
+        <div>
+            <button onClick={props.setInc}> + </button>
+            <button onClick={props.setDec}> - </button>
+
+        </div>
+
+    )
+
+
+}
