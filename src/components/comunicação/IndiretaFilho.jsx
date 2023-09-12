c51df8ec8c0c7b4c85938705e88721f7df1e0177
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-12 08:40:58 -0300

    UPDATE: function to create random values

--- src/components/comunicação/IndiretaFilho.jsx
+++ src/components/comunicação/IndiretaFilho.jsx
@@ -3,14 +3,19 @@ import React from "react";
 
 export default props => {
 
+    const min = 50;
+    const max = 70;
     
+    const gerarIdade = () =>parseInt(Math.random() * (20)) + 50
+
+    const gerarNerd = () =>Math.random() > 0.5
 
     return (
         <div>
             <div>Filho</div>
             <button onClick={
                 function(e){
-                    props.quandoClicar ('joao',43,true)
+                    props.quandoClicar ('joao',gerarIdade(),gerarNerd())
                 }
             }>
                 Fornecer Informações
